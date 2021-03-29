254208eef61c5c4ae4c2053a61640775ed2140b4
Mariah Rivera <rivmariah06@gmail.com>
2021-03-29 11:40:24 -0400

    created objectstore method

--- public/db.js
+++ public/db.js
@@ -1,3 +1,9 @@
 let db;
 // This line creates a new db request for a "budget" database
 const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function (event) {
+  // using the object store method to createObjectStore called "pending" and set autoIncrement to true
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
