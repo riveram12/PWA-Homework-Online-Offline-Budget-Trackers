c57619ba549e8a496b0a0951b9cb58bcb29a2ce2
Mariah Rivera <rivmariah06@gmail.com>
2021-04-07 09:52:33 -0400

    adding .addEventListener for fetch function and event.respondWith

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,4 @@
+//cache files
 const FILES_TO_CACHE = [
   "/",
   "/db.js",
@@ -11,7 +12,7 @@ const FILES_TO_CACHE = [
 const CACHE_NAME = "site-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-// install
+// install steps
 self.addEventListener("install", function (evt) {
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -21,4 +22,45 @@ self.addEventListener("install", function (evt) {
   );
 
   self.skipWaiting();
+});
+
+self.addEventListener("fetch", function (event) {
+  // cache all get requests to /api route
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(event.request)
+            .then((response) => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch((err) => {
+              // if the network request failed, try to get it from the cache.
+              return cache.match(event.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(function () {
+      return caches.match(event.request).then(function (response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          // return the cached home page for all requests for html pages
+          return caches.match("/");
+        }
+      });
+    })
+  );
 });
